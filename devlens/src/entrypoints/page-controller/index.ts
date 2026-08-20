70c79f6c67b1b3a5c7b7ffeab1729cc118c3e553
Zyrn.io <jynxinhaler@gmail.com>
2026-08-20 18:38:43 -0400

    Delete devlens directory

--- devlens/src/entrypoints/page-controller/index.ts
+++
@@ -1,6 +0,0 @@
-import { defineUnlistedScript } from 'wxt/utils/define-unlisted-script';
-import { installPageController } from '../../content/controller';
-
-export default defineUnlistedScript(() => {
-  installPageController();
-});
--- devlens/src/entrypoints/sidepanel/main.tsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import { DevLensApp } from '../../components/DevLensApp';
-import '../../assets/devlens.css';
-
-createRoot(document.getElementById('root')!).render(<DevLensApp />);
